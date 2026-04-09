5f9c65ab14fb069dc7cc17b6ab6c471f6299431c
ZhangBY619 <boyangzhang246@gmail.com>
2026-04-09 20:45:43 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+document.body.innerHTML = '<div style="background:red;color:white;padding:20px;font-size:30px;position:fixed;top:0;z-index:9999">代码已执行</div>';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
